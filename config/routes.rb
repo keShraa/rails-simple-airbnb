7c6a52a943479e6176625de4bb9f7239ccf2d3c2
julien legay <legayj@gmail.com>
2019-08-10 14:51:22 +0200

    routes / controller + actions / seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :flats
+
+  # get     '/flats',           to: 'flats#index', as: :flats
+  # post    '/flats',           to: 'flats#create'
+  # get     '/flats/new',       to: 'flats#new', as: :flats_new
+  # get     '/flats/:id/edit',  to: 'flats#edit', as: :edit_flat
+  # get     '/flats/:id',       to: 'flats#show', as: :flat
+  # patch   '/flats/:id',       to: 'flats#update'
+  # delete  '/flats/:id',       to: 'flats#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Clearing DB...'
+Flat.destroy_all
+
+puts 'Creating 5 flats...'
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Paris',
+  address: '10 Clifton Gardens Paris W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 125,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Berlin',
+  address: '10 Clifton Gardens Berlin W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 65,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Bangkok',
+  address: '10 Clifton Gardens Bangkok W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 135,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Bordeaux',
+  address: '10 Clifton Gardens Bordeaux W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 60,
+  number_of_guests: 2
+)
+
+puts 'Done.'
